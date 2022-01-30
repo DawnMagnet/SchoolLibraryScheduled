d2b77f73af0c165784845465b7df333f290b1083
DawnMagnet <axccjqh@qq.com>
2022-01-30 22:20:48 +0800

    逻辑修正

--- bookStoreInfo.py
+++ bookStoreInfo.py
@@ -4,12 +4,16 @@ import re
 import pandas as pd
 import requests
 import toml
+from requests.exceptions import ConnectTimeout
+
 
 class BookStoreInfo:
     def __init__(self, config_path, debug=False):
 
+        self.df4 = None
+        self.df1 = None
         self.full_data = None
-        self.avai_data = None
+        self.available_data = None
         self.ruled_appointment = None
         self.config_path = config_path
         self.CONFIG = toml.load(config_path)
@@ -17,7 +21,7 @@ class BookStoreInfo:
         self.refresh_available_info()
         self.getRuledAppointment()
 
-    def makeOneAppointment(self, roomId, start_time, remain_hours):
+    def makeOneAppointment(self, room_id, start_time, remain_hours):
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         begin_time = str(start_time * 60)
         end_time = str((start_time + remain_hours) * 60)
@@ -26,34 +30,35 @@ class BookStoreInfo:
             'JSESSIONID': self.CONFIG["JSESSIONID"],
         }
         referer = f'http://libwx.cau.edu.cn/space/discuss/openAppointDetail?\
-                  roomid={roomId}&ustime={begin_time}&uetime={end_time}\
+                  roomid={room_id}&ustime={begin_time}&uetime={end_time}\
                   &selectDate={today}&ruleId={ruleId}&\
                   mobile=true&linkSign=discuss'
         headers = {
-            'User-Agent'      : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1 Edg/95.0.4638.54',
-            'Accept'          : 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language' : 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-            'Content-Type'    : 'application/json',
-            'X-CSRF-TOKEN'    : self.CONFIG['X_CSRF_TOKEN'],
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, '
+                          'like Gecko) Version/10.0 Mobile/14E304 Safari/602.1 Edg/95.0.4638.54',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': self.CONFIG['X_CSRF_TOKEN'],
             'X-Requested-With': 'XMLHttpRequest',
-            'Origin'          : 'http://libwx.cau.edu.cn',
-            'Connection'      : 'keep-alive',
-            'Referer'         : referer,
-            'Pragma'          : 'no-cache',
-            'Cache-Control'   : 'no-cache',
+            'Origin': 'http://libwx.cau.edu.cn',
+            'Connection': 'keep-alive',
+            'Referer': referer,
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
         }
         data = str({
-            "_stime"     : begin_time,
-            "_etime"     : end_time,
-            "_roomid"    : roomId,
+            "_stime": begin_time,
+            "_etime": end_time,
+            "_roomid": room_id,
             "_currentday": today,
-            "UUID"       : "VEmkgCYM",
-            "ruleId"     : ruleId,
-            "users"      : "2019307070109 2019321010102",
-            "usercount"  : "2",
-            "room_exp"   : "[]",
-            "_seatno"    : "0",
-            "LOCK"       : "true"
+            "UUID": "VEmkgCYM",
+            "ruleId": ruleId,
+            "users": "2019307070109 2019321010102",
+            "usercount": "2",
+            "room_exp": "[]",
+            "_seatno": "0",
+            "LOCK": "true"
         }).replace("'", '"')
         return requests.post('http://libwx.cau.edu.cn/space/form/dynamic/saveFormLock',
                              headers=headers, cookies=cookies, data=data)
@@ -68,7 +73,8 @@ class BookStoreInfo:
             available_period = []
             for hour in range(8, 22):
                 if self.full_data.loc[roomId][str(hour)] == 'O':
-                    if len(available_period) > 0 and len(available_period[-1]) < 3 and available_period[-1][-1] == hour - 1:
+                    if len(available_period) > 0 and len(available_period[-1]) < 3 \
+                            and available_period[-1][-1] == hour - 1:
                         available_period[-1].append(hour)
                     else:
                         available_period.append([hour])
@@ -78,7 +84,7 @@ class BookStoreInfo:
                 roomId, available_time_period[0], len(available_time_period))
             try:
                 res_dict[str(available_time_period)] = res.json()
-            except Exception as e:
+            except Exception as _:
                 res_dict[str(available_time_period)] = 'json parse error'
         return res_dict
 
@@ -89,15 +95,16 @@ class BookStoreInfo:
         }
         headers = {
             'Proxy-Connection': 'keep-alive',
-            'Accept'          : '*/*',
-            'DNT'             : '1',
-            'X-CSRF-TOKEN'    : self.CONFIG['X_CSRF_TOKEN'],
+            'Accept': '*/*',
+            'DNT': '1',
+            'X-CSRF-TOKEN': self.CONFIG['X_CSRF_TOKEN'],
             'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent'      : 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
-            'Content-Type'    : 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Origin'          : 'http://libwx.cau.edu.cn',
-            'Referer'         : 'http://libwx.cau.edu.cn/space/discuss/myAppoint?linkSign=myReserve&type=discuss',
-            'Accept-Language' : 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': 'http://libwx.cau.edu.cn',
+            'Referer': 'http://libwx.cau.edu.cn/space/discuss/myAppoint?linkSign=myReserve&type=discuss',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
         }
         data = {
             'id': appoint_id
@@ -118,12 +125,12 @@ class BookStoreInfo:
 
         headers = {
             'Proxy-Connection': 'keep-alive',
-            'Accept'          : '*/*',
-            'DNT'             : '1',
+            'Accept': '*/*',
+            'DNT': '1',
             'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent'      : 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
-            'Referer'         : 'http://libwx.cau.edu.cn/space/discuss/myAppoint?linkSign=myReserve&type=discuss',
-            'Accept-Language' : 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
+            'Referer': 'http://libwx.cau.edu.cn/space/discuss/myAppoint?linkSign=myReserve&type=discuss',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
         }
 
         params = (
@@ -170,17 +177,24 @@ class BookStoreInfo:
                 url=url,
                 headers={
                     "X-CSRF-TOKEN": self.CONFIG['X_CSRF_TOKEN'],
-                    "Cookie"      : f"JSESSIONID={self.CONFIG['JSESSIONID']}"
+                    "Cookie": f"JSESSIONID={self.CONFIG['JSESSIONID']}"
                 },
                 data={
                     "currentPage": 1,
-                    "pageSize"   : 100
-                }
+                    "pageSize": 100
+                },
+                timeout=5.0
             ).json()
             df = pd.DataFrame(res["params"]["rooms"]["pageList"])
             ruleId = res["params"]["ruleId"]
             return True, df, ruleId
-        except Exception as _:
+
+        except ConnectTimeout as _:
+            print("[ERROR] NetWork Error! Please Check Connections Between Host and Server!")
+            exit(0)
+        except Exception as e:
+            print(e)
+            print(type(e))
             return False, None, None
 
     def write_toml(self):
@@ -209,7 +223,7 @@ class BookStoreInfo:
         self.df = pd.DataFrame(pd.concat([self.df1, self.df4], axis=0))
         self.full_data = self.deal_available_info(self.df.copy())
         self.full_data.to_csv("full_data.csv")
-        self.avai_data = self.deal_available_info(
+        self.available_data = self.deal_available_info(
             self.df.copy(), available_filter=True)
 
     def deal_available_info(self, df, available_filter=False):
@@ -241,12 +255,12 @@ class BookStoreInfo:
             roomName = self.ruled_appointment['rname'].values[-1]
             roomId = self.full_data[self.full_data['rname'] == roomName].index.values[-1]
         headers = {
-            'Proxy-Connection'         : 'keep-alive',
-            'DNT'                      : '1',
+            'Proxy-Connection': 'keep-alive',
+            'DNT': '1',
             'Upgrade-Insecure-Requests': '1',
-            'User-Agent'               : 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
-            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language'          : 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36 Edg/95.0.1020.40',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
         }
 
         params = self.CONFIG[sign_config]
@@ -262,13 +276,13 @@ class BookStoreInfo:
 
     def get_new_cookies(self):
         common_headers = {
-            'Connection'               : 'keep-alive',
-            'DNT'                      : '1',
-            'User-Agent'               : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+            'Connection': 'keep-alive',
+            'DNT': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
             'Upgrade-Insecure-Requests': '1',
-            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language'          : 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'Cache-Control'            : 'max-age=0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'Cache-Control': 'max-age=0',
 
         }
 
@@ -311,6 +325,7 @@ class BookStoreInfo:
             r'name="_csrf" content="(?P<CUR>.*)"', response.text).groupdict()['CUR']
         return [JSESSIONID, X_CSRF_TOKEN]
 
+
 def desensitize(data: pd.DataFrame):
     data['rname'] = data['rname'].map(lambda x: x.replace(
         '层', '-').replace(b'\xe5\x8c\xba\xe4\xba\xa4\xe6\xb5\x81\xe7\xa9\xba\xe9\x97\xb4'.decode('utf-8'), '-').replace(
@@ -318,6 +333,7 @@ def desensitize(data: pd.DataFrame):
     if 'times' in data.columns:
         del data['times']
     return data
+
 
 def dprint(data):
     print(desensitize(data.copy()))
--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def cur_time_str():
     import time
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+
 def make_new_line():
     print("> ", end='')
+
 
 @scheduler.scheduled_job('cron', minute='5, 30', hour='7-22', id="refresh", max_instances=100)
 def refresh():
@@ -26,16 +28,18 @@ def refresh():
     print(cur_time_str(), "user A", bi.sign('SIGN_PARAM'))
     print(cur_time_str(), "user B", bi.sign('SIGN_PARAM_2'))
     make_new_line()
-    
+
 
 @scheduler.scheduled_job('cron', hour='0', minute='0', second='0', id='nxt_day_app')
 def scheduled_appointment(seat=None):
     bi.__init__("config.toml")
-    res = bi.makeOneSeatEveryAppointment(roomId=seat,force=True)
+    res = bi.makeOneSeatEveryAppointment(roomId=seat, force=True)
     print("[SCHEDULED RESULT]")
     for time_period in res.keys():
         print("[{}]{} {} {}".format(cur_time_str(), time_period,
                                     res[time_period]['status'], res[time_period]['content']))
+
+
 # @scheduler.scheduled_job('cron', hour='23', minute='59', second='10,20,30,40,50,55', id='refresh_nxt_day')
 # def refresh_nxt_day():
 #     bi.__init__("config.toml")
@@ -55,15 +59,15 @@ if __name__ == "__main__":
     help: print this help message
     exit: exit the program
     la  : print school library full list
-    ls  : print school library avai list
+    ls  : print school library available list
     ap  : print appointments list
     jb  : print background jobs(debug)
     r   : force refresh info(debug)
-    cs  : cancel sched
+    cs  : cancel schedule
     sg  : sign(force)
     st  : set current seat
-    sn  : sched now(force)
-    s   : sched next_day
+    sn  : schedule now(force)
+    s   : schedule next_day
                 """.format(cur_time_str()))
             elif command[0] == "exit":
                 scheduler.shutdown(wait=False)
@@ -72,7 +76,7 @@ if __name__ == "__main__":
             elif command[0] == "la":
                 dprint(bi.full_data)
             elif command[0] == "ls":
-                dprint(bi.avai_data)
+                dprint(bi.available_data)
             elif command[0] == "ap":
                 dprint(bi.ruled_appointment)
             elif command[0] == "r":
@@ -87,13 +91,14 @@ if __name__ == "__main__":
                     print(bi.sign())
             elif command[0] == "s":
                 from datetime import datetime
+
                 app_time = datetime.now()
                 app_time = app_time.replace(
                     day=app_time.day + 1, hour=0, minute=0, second=2, microsecond=0)
                 # app_time = app_time.replace(second=app_time.second + 1)
                 print("Job Will Start At {}.".format(app_time))
                 scheduler.add_job(scheduled_appointment, 'date',
-                                    run_date=app_time, id='nxt_day_app')
+                                  run_date=app_time, id='nxt_day_app')
             elif command[0] == "sn":
                 if len(command) > 1:
                     scheduled_appointment(command[1])
